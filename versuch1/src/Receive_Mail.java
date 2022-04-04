17cbaa155e70ccd3dd0ec2a74ffa298a80e4b9fb
deco42 <82336539+deco42@users.noreply.github.com>
2022-04-04 08:13:23 +0200

    Update Receive_Mail.java
    Co-authored-by: MGZeroes <MGZeroes@users.noreply.github.com>
    Co-authored-by: MrRaptorious <MrRaptorious@users.noreply.github.com>
    Co-authored-by: elibright1 <elibright1@users.noreply.github.com>

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -1,8 +1,7 @@
+import java.util.Date;
 import java.util.Properties;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Store;
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
 
 public class Receive_Mail {
 	public static void main(String[] args) throws Exception {
@@ -11,7 +10,37 @@ public class Receive_Mail {
 	
 	public static void fetchMail() {
 		try {
-			// your code here
+			Properties props = new Properties();
+			props.put("mail.store.protocol", "pop3");
+			props.put("mail.pop3.host", "localhost");
+			//props.put("mail.from", "labrat@localhost");
+			//props.put("mail.debug", "true");
+			//props.put("mail.debug.quote", "true");
+			Session session = Session.getInstance(props, null);
+			//session.setDebug(true);
+
+			Store store = session.getStore();
+			store.connect("localhost", "labrat", "kn1lab");
+
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+
+			Message[] messages = folder.getMessages();
+
+			int icount = 1;
+			for (Message msg : messages)
+			{
+				System.out.println("Email #" + (icount++) + ":");
+				System.out.println("--------------");
+				System.out.println("\tSubject : " + msg.getSubject());
+				System.out.println("\tFrom : " + msg.getFrom()[0]);
+				System.out.println("\tDate : " + msg.getSentDate());
+				System.out.println("--------------");
+				System.out.println("\tBody : " +  msg.getContent());
+			}
+			
+			folder.close();
+			store.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
