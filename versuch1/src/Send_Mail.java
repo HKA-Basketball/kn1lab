fdd85d76b9406d981582671411d34635f2339885
deco42 <82336539+deco42@users.noreply.github.com>
2022-04-04 08:14:33 +0200

    Update Send_Mail.java
    Co-authored-by: MGZeroes <MGZeroes@users.noreply.github.com>
    Co-authored-by: MrRaptorious <MrRaptorious@users.noreply.github.com>
    Co-authored-by: elibright1 <elibright1@users.noreply.github.com>

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,3 +1,7 @@
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -5,7 +9,8 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage; 
+import javax.mail.internet.MimeMessage;
+import javax.naming.InitialContext;
 
 public class Send_Mail {
 	public static void main(String[] args) {
@@ -14,7 +19,23 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "localhost");
+			Session session = Session.getInstance(props, null);
+
+			MimeMessage msg = new MimeMessage(session);
+			msg.setFrom("labrat@localhost");
+			msg.setRecipients(Message.RecipientType.TO,"labrat@localhost");
+			msg.setSubject("42");
+			msg.setSentDate(new Date());
+			msg.setText("#42 Hello, world! #42\n");
+
+			Transport.send(msg, "labrat@localhost", "kn1lab");
+
+			System.out.println("Subject : " + msg.getSubject());
+			System.out.println("From : " + msg.getFrom()[0]);
+			System.out.println("--------------");
+			System.out.println("Body : " +  msg.getContent());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
